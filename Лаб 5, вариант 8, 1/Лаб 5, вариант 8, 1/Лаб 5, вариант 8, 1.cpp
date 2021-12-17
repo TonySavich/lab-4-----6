f94ec84b93247a68e7dd6812ed440e74b3e73b6f
Anton <antonsavich19@gmail.com>
2021-12-17 20:28:43 +0200

    .

---
+++ Лаб 5, вариант 8, 1/Лаб 5, вариант 8, 1/Лаб 5, вариант 8, 1.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+    int k, q;
+    q = 0;
+    k = s.length();
+    for (int i = 0; i < k; i++) {
+        if ((s[i] == 46)||(s[i]== ';')||(s[i]==',')||(s[i] == ':')||(s[i] == '-')||(s[i] == '?')||(s[i] == '!')||(s[i] == '(')||(s[i] == ')')||(s[i] == '"')||(s[i] == 39)) {
+            q = q + 1;
+
+        }
+    }
+    std::cout << q;
+
+}
+
