d8a571f811071f268e8b98eda0b38ed693cd9fc0
Anton <antonsavich19@gmail.com>
2021-12-17 20:31:25 +0200

    .

---
+++ лабораторная работа 4, вариант 8, 2/лабораторная работа 4, вариант 8, 2/лабораторная работа 4, вариант 8, 2.cpp
@@ -0,0 +1,28 @@
+﻿// лабораторная работа 4, вариант 8, 2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	int p, k, l;
+	k = 0;
+	l = 0;
+	p = 0;
+	const int y = 5;
+	int mas[y];
+	for (int i = 0; i < y; i++)
+	{
+		std::cin >> mas[i];
+	}
+
+	for (int i = 0; i < y; i++) {
+		for (int j = i + 1; j < y; j++) {
+			
+
+		}
+	}
+}
+
+
+
